gentoo-mirror/gentoo

commit 1afd8bcd3b973e319a985c67242ce9e66eb17bc2

sci-geosciences/gpsd: add 3.27.5, wire up verify-sig
Drop qt6 patch, merged upstream. Enable py3.14

Signed-off-by: Brett A C Sheffield <bacs@librecast.net>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/745
Signed-off-by: Sam James <sam@gentoo.org>

sci-geosciences/gpsd/gpsd-3.27.5.ebuild
@@ -1,0 +1,244 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DISTUTILS_OPTIONAL=1
+DISTUTILS_USE_PEP517=setuptools
+PYTHON_COMPAT=( python3_{11..14} )
+SCONS_MIN_VERSION="2.3.0"
+
+inherit distutils-r1 scons-utils systemd toolchain-funcs udev
+
+if [[ ${PV} == 9999 ]] ; then
+	EGIT_REPO_URI="https://gitlab.com/gpsd/gpsd.git"
+	inherit git-r3
+else
+	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/garyemiller.asc
+	inherit verify-sig
+	SRC_URI="
+		mirror://nongnu/${PN}/${P}.tar.xz
+		verify-sig? ( mirror://nongnu/${PN}/${P}.tar.xz.sig )
+	"
+	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
+
+fi
+
+DESCRIPTION="GPS daemon and library for USB/serial GPS devices and GPS/mapping clients"
+HOMEPAGE="https://gpsd.gitlab.io/gpsd/"
+
+LICENSE="BSD-2"
+SLOT="0/31"
+
+GPSD_PROTOCOLS=(
+	aivdm ashtech earthmate evermore fury fv18 garmin garmintxt geostar
+	gpsclock greis isync itrax navcom nmea2000 oncore
+	sirf skytraq superstar2 tnt tripmate tsip
+)
+IUSE_GPSD_PROTOCOLS=${GPSD_PROTOCOLS[@]/#/+gpsd_protocols_}
+IUSE="${IUSE_GPSD_PROTOCOLS} bluetooth +cxx dbus debug latency-timing ncurses ntp qt6 selinux +shm static systemd test udev usb X"
+REQUIRED_USE="
+	gpsd_protocols_nmea2000? ( gpsd_protocols_aivdm )
+	${PYTHON_REQUIRED_USE}
+	qt6? ( cxx )
+"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	acct-user/gpsd
+	acct-group/dialout
+	>=net-misc/pps-tools-0.0.20120407
+	bluetooth? ( net-wireless/bluez:= )
+	dbus? (
+		sys-apps/dbus
+		dev-libs/dbus-glib
+	)
+	ncurses? ( sys-libs/ncurses:= )
+	ntp? ( || (
+		net-misc/ntp
+		net-misc/ntpsec
+		net-misc/chrony
+	) )
+	qt6? ( dev-qt/qtbase:6[network] )
+	${PYTHON_DEPS}
+	dev-python/pyserial[${PYTHON_USEDEP}]
+	usb? ( virtual/libusb:1 )
+	X? ( dev-python/pygobject:3[cairo,${PYTHON_USEDEP}] )"
+DEPEND="${RDEPEND}"
+BDEPEND="virtual/pkgconfig
+	$(python_gen_any_dep 'dev-build/scons[${PYTHON_USEDEP}]')
+	${DISTUTILS_DEPS}
+	test? ( app-alternatives/bc )"
+RDEPEND+=" selinux? ( sec-policy/selinux-gpsd )"
+
+# asciidoctor package is for man page generation
+if [[ ${PV} == *9999* ]] ; then
+	BDEPEND+=" dev-ruby/asciidoctor"
+fi
+
+python_check_deps() {
+	python_has_version -b "dev-build/scons[${PYTHON_USEDEP}]" || return 1
+}
+
+src_prepare() {
+	# Make sure our list matches the source.
+	local src_protocols=$(echo $(
+		sed -n '/# GPS protocols/,/# Time service/{s:#.*::;s:[(",]::g;p}' \
+		 "${S}"/SConscript | awk '{print $1}' | LC_ALL=C sort
+	) )
+
+	if [[ ${src_protocols} != ${GPSD_PROTOCOLS[*]} ]] ; then
+		eerror "Detected protocols: ${src_protocols}"
+		eerror "Ebuild protocols:   ${GPSD_PROTOCOLS[*]}"
+		die "please sync ebuild & source"
+	fi
+
+	default
+
+	distutils-r1_src_prepare
+}
+
+python_prepare_all() {
+	python_setup
+
+	# bug #796476
+	python_export_utf8_locale
+
+	# Extract python info out of SConscript so we can use saner distribute
+	pyarray() { sed -n "/^ *$1 *= *\\[/,/\\]/p" SConscript ; }
+	local pyprogs=$(pyarray python_progs)
+	local pybins=$("${PYTHON}" -c "${pyprogs}; \
+		print(list(set(python_progs) - {'xgps', 'xgpsspeed', 'ubxtool', 'zerk'}))" || die "Unable to list pybins")
+	# Handle conditional tools manually. #666734
+	use X && pybins+="+ ['xgps', 'xgpsspeed']"
+	pybins+="+ ['ubxtool']"
+	use gpsd_protocols_greis && pybins+="+ ['zerk']"
+	local pysrcs=$(pyarray packet_ffi_extension)
+	local packet=$("${PYTHON}" -c "${pysrcs}; print(packet_ffi_extension)" || die "Unable to extract packet types")
+
+	pyvar() { sed -n "/^ *$1 *=/s:.*= *::p" SConscript ; }
+	pyvar2() { sed -n "/^ *$1 *=/s:.*= *::p" SConstruct ; }
+
+	# Post 3.19 the clienthelpers were merged into gps.packet
+
+	# TODO: Fix hardcoding https://gpsd.io/ for now for @URL@
+	sed \
+		-e "s|@VERSION@|$(pyvar2 gpsd_version | sed -e 's:\"::g')|" \
+		-e "s|@URL@|https://gpsd.io/|" \
+		-e "s|@DEVMAIL@|$(pyvar devmail)|" \
+		-e "s|@SCRIPTS@|${pybins}|" \
+		-e "s|@DOWNLOAD@|$(pyvar download)|" \
+		-e "s|@IRCCHAN@|$(pyvar ircchan)|" \
+		-e "s|@ISSUES@|$(pyvar bugtracker)|" \
+		-e "s|@MAILMAN@|$(pyvar mailman)|" \
+		-e "s|@PROJECTPAGE@|$(pyvar projectpage)|" \
+		-e "s|@SUPPORT@|https://gpsd.io/SUPPORT.html|" \
+		-e "s|@WEBSITE@|https://gpsd.io/|" \
+		"${S}"/packaging/gpsd-setup.py.in > setup.py || die
+
+	if [[ "${PV}" == *9999* ]]; then
+		# Distutils doesn't like the tilde
+		sed -i s/~dev/-dev/ setup.py || die
+	fi
+
+	distutils-r1_python_prepare_all
+}
+
+src_configure() {
+	scons_opts=(
+		prefix="${EPREFIX}/usr"
+		libdir="\$prefix/$(get_libdir)"
+		udevdir="$(get_udevdir)"
+		rundir="/run"
+		chrpath=False
+		gpsd_user=gpsd
+		gpsd_group=dialout
+		nostrip=True
+		systemd=$(usex systemd)
+		unitdir="$(systemd_get_systemunitdir)"
+		shared=$(usex !static True False)
+		bluez=$(usex bluetooth)
+		libgpsmm=$(usex cxx)
+		clientdebug=$(usex debug)
+		dbus_export=$(usex dbus)
+		timing=$(usex latency-timing)
+		ncurses=$(usex ncurses)
+		ntpshm=$(usex ntp)
+		pps=$(usex ntp)
+		# force a predictable python libdir because lib vs. lib64 usage differs
+		# from 3.5 to 3.6+
+		python_libdir="${EPREFIX}"/python-discard
+		qt=$(usex qt6)
+		shm_export=$(usex shm)
+		socket_export=True # Required, see bug #900891
+		usb=$(usex usb)
+	)
+
+	if [[ ${PV} != *9999* ]] ; then
+		scons_opts+=( manbuild=False )
+	fi
+
+	use X && scons_opts+=( xgps=1 xgpsspeed=1 )
+	use qt6 && scons_opts+=( qt_versioned=6 )
+
+	# enable specified protocols
+	local protocol
+	for protocol in ${GPSD_PROTOCOLS[@]} ; do
+		scons_opts+=( ${protocol}=$(usex gpsd_protocols_${protocol}) )
+	done
+
+	# bug #809260
+	python_setup
+}
+
+src_compile() {
+	export CHRPATH=
+	tc-export CC CXX PKG_CONFIG
+	export SHLINKFLAGS=${LDFLAGS} LINKFLAGS=${LDFLAGS}
+	escons "${scons_opts[@]}"
+
+	pushd "${PN}"-* || die
+	ln -sf ../setup.py . || die
+	distutils-r1_src_compile
+	popd || die
+}
+
+src_test() {
+	escons "${scons_opts[@]}" check
+}
+
+python_test() {
+	# Silence QA check which gets confused by layout(?). We do run the tests.
+	:;
+}
+
+python_install() {
+	while read -d '' -r file ; do
+		grep -q "#!/usr/bin/env python" "${file}" && python_doscript "${file}"
+	done < <(find "${T}"/scripts -type f -print0)
+
+	distutils-r1_python_install
+}
+
+src_install() {
+	DESTDIR="${D}" escons install "${scons_opts[@]}" $(usev udev udev-install)
+
+	newconfd "${FILESDIR}"/gpsd.conf-2 gpsd
+	newinitd "${FILESDIR}"/gpsd.init-2 gpsd
+
+	# Cleanup bad alt copy due to Scons
+	rm -rf "${D}"/python-discard/gps*
+	find "${D}"/python-discard/ -type d -delete
+
+	# Install correct multi-python copy
+	pushd "${PN}"-* || die
+	mkdir -p "${T}/scripts" || die
+	grep -Rl "${D}/usr/bin" -e "/usr/bin/env python" | xargs cp -t "${T}/scripts"
+	assert "Moving Python scripts failed"
+	distutils-r1_src_install
+	popd || die
+}
+
+pkg_postinst() {
+	use udev && udev_reload
+}

sci-geosciences/gpsd/Manifest
@@ -1,1 +1,3 @@
 DIST gpsd-3.26.1.tar.xz 4049692 BLAKE2B fcbe57e9bd5a45292ae5bfa2d6b0db4162bc73a014fa116304fdd9be34a473bdcc476e4c8f5c4b361f0a7b9c830bca12eb076c26ea94b357cfef2aecf1a6fee2 SHA512 2c8f4b22a2ee9af7f3630a0043aaecc61ebe458a61fbdce52073e5aae198f7d5b139b3c291f5bd4b99dacbfb43c74312b377b182c7b915f3ec6638ebc21b986d
+DIST gpsd-3.27.5.tar.xz 4083248 BLAKE2B 26cd7bf97442690da6f93c3c7a42cc5a32f8affe5fa0cdda3a6a4f543bd19a7b4ff34633445a6d9d3d24cb4c225ab9659bf36150c2799030258df6e78f5a1ea6 SHA512 bf0dda3c81761f03f64d6f36a0ecc283402b80c754f3b1d116d249698f9517958b73bfc1409af72ddbc4502f93f4f6f797b0b94d13df4f27d11b29cd55c4a203
+DIST gpsd-3.27.5.tar.xz.sig 147 BLAKE2B e3dead6a6fdb51025a1c72f432e88c2941162f75119751e449534d881f4d00b2d9371c85d186f563ccbca27fa773cbdcf62158268f2c87af84149634e8fec216 SHA512 b77be8f272294f1bc35a8684ce4b52d90578e2c7a05ff1581c263b69046f3aa558c538568e22827f06bc9db60cfb562d92d3b89d2f4ac35a946a9b9bdaaf17c7

commit 74f17b20695fb88c4cb12fc7434bcdb445f59920

sci-geosciences/gpsd: fix BDEPEND for verify-sig
Closes: https://bugs.gentoo.org/973332
Fixes: 1afd8bcd3b973e319a985c67242ce9e66eb17bc2
Signed-off-by: Brett A C Sheffield <bacs@librecast.net>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/763
Merges: https://codeberg.org/gentoo/gentoo/pulls/763
Signed-off-by: Sam James <sam@gentoo.org>

sci-geosciences/gpsd/gpsd-3.27.5.ebuild
@@ -21,7 +21,7 @@ else
 		verify-sig? ( mirror://nongnu/${PN}/${P}.tar.xz.sig )
 	"
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
-
+	BDEPEND="verify-sig? ( sec-keys/openpgp-keys-garyemiller ) "
 fi
 
 DESCRIPTION="GPS daemon and library for USB/serial GPS devices and GPS/mapping clients"
@@ -65,7 +65,7 @@ RDEPEND="
 	usb? ( virtual/libusb:1 )
 	X? ( dev-python/pygobject:3[cairo,${PYTHON_USEDEP}] )"
 DEPEND="${RDEPEND}"
-BDEPEND="virtual/pkgconfig
+BDEPEND+="virtual/pkgconfig
 	$(python_gen_any_dep 'dev-build/scons[${PYTHON_USEDEP}]')
 	${DISTUTILS_DEPS}
 	test? ( app-alternatives/bc )"

sci-geosciences/gpsd/gpsd-9999.ebuild
@@ -21,7 +21,7 @@ else
 		verify-sig? ( mirror://nongnu/${PN}/${P}.tar.xz.sig )
 	"
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
-
+	BDEPEND="verify-sig? ( sec-keys/openpgp-keys-garyemiller ) "
 fi
 
 DESCRIPTION="GPS daemon and library for USB/serial GPS devices and GPS/mapping clients"
@@ -65,7 +65,7 @@ RDEPEND="
 	usb? ( virtual/libusb:1 )
 	X? ( dev-python/pygobject:3[cairo,${PYTHON_USEDEP}] )"
 DEPEND="${RDEPEND}"
-BDEPEND="virtual/pkgconfig
+BDEPEND+="virtual/pkgconfig
 	$(python_gen_any_dep 'dev-build/scons[${PYTHON_USEDEP}]')
 	${DISTUTILS_DEPS}
 	test? ( app-alternatives/bc )"