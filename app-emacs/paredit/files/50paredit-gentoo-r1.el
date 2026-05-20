09b5952831249516181f8ca994453ec0a65fc47b
Maciej Barć <xgqt@gentoo.org>
2026-05-20 17:45:52 +0200

    app-emacs/paredit: remove default site-lisp hooks
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ app-emacs/paredit/files/50paredit-gentoo-r1.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+(autoload 'enable-paredit-mode "paredit" nil t)
