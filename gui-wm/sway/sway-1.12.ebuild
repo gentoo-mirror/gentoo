gentoo-mirror/gentoo

commit a880a6dc9e43ef0143688804526fcc77e6093567

gui-wm/sway: add 1.12
Signed-off-by: RadioNoiseE <rne.kou@icloud.com>
Part-of: https://github.com/gentoo/gentoo/pull/46310
Closes: https://github.com/gentoo/gentoo/pull/46310
Signed-off-by: Matthew Thode <prometheanfire@gentoo.org>

gui-wm/sway/sway-1.12.ebuild
@@ -16,7 +16,7 @@ else
 	inherit verify-sig
 	SRC_URI="https://github.com/swaywm/${PN}/releases/download/${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
 		https://github.com/swaywm/${PN}/releases/download/${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
-	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc64 ~riscv ~x86"
+	KEYWORDS="amd64 ~arm arm64 ~loong ~ppc64 ~riscv x86"
 	S="${WORKDIR}/${PN}-${MY_PV}"
 fi
 

gui-wm/sway/Manifest
@@ -4,5 +4,7 @@ DIST sway-1.10.gh.tar.gz 5587460 BLAKE2B 91cbbf87f1cf4764fd58bbf85efea1c5d502a57
 DIST sway-1.10.gh.tar.gz.sig 566 BLAKE2B 24293ffe98a93775a23a580f4acff22efe7e0f8ecf3b6732d4f91043fa2769ae515bdb673f64ea33e9e134baf9cb9bb549fdc126aff5a10d21140479957fb7e1 SHA512 015e09b3d2c383e03d128d5192f58c540cb9347a2adb3a70b77e971de6fca136d1e6fb5477968130029c093996049981c5cdf3d3077ec5da46b0f03c7b8ca389
 DIST sway-1.11.gh.tar.gz 5630956 BLAKE2B 98b1f1788df3cd67577f5b3b0db015c27f9a2770ad110c8590bf3f4a35500ec69cf6286b932e63598723e5d190cc9f4ef5711e03aa35bd381fb5d502d09c0c04 SHA512 995810f678b19067430a4c6aaa969607bd8dbae744d5aea9a3dbe24634f494931a7d902d9ea534e4ffa326983c8dca841b2d1b08e980ed95a4c66a0b09a02e72
 DIST sway-1.11.gh.tar.gz.sig 566 BLAKE2B c8c5bfdd9a25e45a165b9124d75f35e51b6e3930f6d0d9c935abef6d0c6234672637d876e75d7138a3dc420770c77ca5cb0fc8d7a04f55839d32cea66591af5f SHA512 e8e65da3526a7bc2bc86cb9ac8efc12219931847fe171c6ad9d65b42295a3b6a7eff6e2be969e7982c2b530220e8c72560a2020e32ecf0a4e4a7e9f062f1dd5a
+DIST sway-1.12.gh.tar.gz 5637846 BLAKE2B d691ef6e44eaa63d0ae5856a60f883b4e7f4724ecb5b709aeb0657e500dc7fefe1bcb6f9c3b13a0c54a272e406b78da25540f0d7c214734cf704b4c76ef969ff SHA512 35e0843c6bc24ef90c50b45382b8765bdd6318df4716f37c76e10c3ad78d06a826b768519b6ccc262b89bbdbb1732b102d006844b16ec48ba8a427ce2279bb5e
+DIST sway-1.12.gh.tar.gz.sig 566 BLAKE2B c5c2918675388549b58578e89bdcad32cc56ed9a145debceaf755774499003a98f8d99379ff360bacb47d224262c5cdcdeaf098cce81c49ac5aa52aec3a6f87f SHA512 21e8fb9e9996816e0c040cc3823ab8d03bd6262d9e2fdab503e3b2d267ddee2d8834986700119850b5e8cfac8c4be53c06e26f8f501b8ac34bd86d292a5d5cdf
 DIST sway-1.9.gh.tar.gz 5587501 BLAKE2B c6ca46c4c046ee690240a58231acc47bdc562adb2f23522a41cc14fba0cfa14fd4bd03d990640b1a832f862432a6c8ff9518fccf4f1e63ce833c60cd1bf82107 SHA512 1d2a47bb8b838573a32f3719a7329fd744119c2c7efc5e5a4168b2bacfb09a3901a569177e5e10c129141fafe00e823ab78c04b76b502d23caa7621bbccd5919
 DIST sway-1.9.gh.tar.gz.sig 566 BLAKE2B 671ca15d9113bbe586a2546967e9afbcd5927103b86395fe751bfbf7778a2c05805f3fe52a951c34dc18332d653cb8a758c3bada42e9b76fe3f360dd712201cc SHA512 606f3ee05fb2e4e127438fcbed09119b8884a620bfc22d531ec4a5e2fc71a2fb85840a7ab80f2c5cc5ad0ed1b17b7762f617385a596c9cd5ce5bd07987b49776

commit 71be219585727bafcbc82ec41a97fcf839afd60d

gui-wm/sway: destabilize 1.12 for ~amd64, ~arm64, ~x86
It was pushed as stable.

Fixes: a880a6dc9e43ef0143688804526fcc77e6093567
Signed-off-by: Sam James <sam@gentoo.org>

gui-wm/sway/sway-1.12.ebuild
@@ -16,7 +16,7 @@ else
 	inherit verify-sig
 	SRC_URI="https://github.com/swaywm/${PN}/releases/download/${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
 		https://github.com/swaywm/${PN}/releases/download/${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
-	KEYWORDS="amd64 ~arm arm64 ~loong ~ppc64 ~riscv x86"
+	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc64 ~riscv ~x86"
 	S="${WORKDIR}/${PN}-${MY_PV}"
 fi
 