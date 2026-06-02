gentoo-mirror/gentoo

commit 9594e6e0aeafcf17f4934e41e3d49d9392b31392

app-containers/docker-compose: move docker-credential-helpers to optfeature
The dep was added in b1253c00616004482cdd62b336432186b19eef8e but it
isn't strictly needed at runtime. Make it an optfeature instead for
those who want/need credential support (getting a password or token
from a keyring or password manager).

Note that this means 5.1.4 has to be EAPI 8 now for optfeature.eclass,
but there's no rush for EAPI 9, so whatever.

Bug: https://bugs.gentoo.org/971103
Closes: https://bugs.gentoo.org/972411
Signed-off-by: Sam James <sam@gentoo.org>

app-containers/docker-compose/docker-compose-5.1.4-r1.ebuild
@@ -1,7 +1,9 @@
 # Copyright 2018-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=9
+EAPI=8
+
+inherit optfeature
 
 DESCRIPTION="Multi-container orchestration for Docker"
 HOMEPAGE="https://github.com/docker/compose"
@@ -19,7 +21,6 @@ RDEPEND="
 		>=app-containers/docker-cli-29.5.2
 		app-containers/podman[wrapper(+)]
 	)
-	app-containers/docker-credential-helpers
 "
 BDEPEND=">=dev-lang/go-1.25.5"
 
@@ -38,6 +39,8 @@ src_install() {
 }
 
 pkg_postinst() {
+	optfeature "credential store support" app-containers/docker-credential-helpers
+
 	if ver_replacing -lt 2; then
 		ewarn
 		ewarn "docker-compose 2.x is a sub command of docker"

app-containers/docker-compose/docker-compose-5.0.1-r2.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
-inherit eapi9-ver go-module
+inherit eapi9-ver go-module optfeature
 MY_PV=${PV/_/-}
 
 DESCRIPTION="Multi-container orchestration for Docker"
@@ -21,7 +21,6 @@ RDEPEND="
 	>=app-containers/docker-cli-23.0.0
 	app-containers/podman[wrapper(+)]
 )
-app-containers/docker-credential-helpers
 "
 BDEPEND=">=dev-lang/go-1.24.7"
 
@@ -42,6 +41,8 @@ src_install() {
 }
 
 pkg_postinst() {
+	optfeature "credential store support" app-containers/docker-credential-helpers
+
 	if ver_replacing -lt 2; then
 		ewarn
 		ewarn "docker-compose 2.x is a sub command of docker"

commit 32e5c1931f7e96a45870274a2f13b48898339c16

app-containers/docker-compose: add eapi9-ver inherit
In 9594e6e0aeafcf17f4934e41e3d49d9392b31392, I had to downgrade to EAPI 8
for the optfeature but didn't recheck it.

Closes: https://bugs.gentoo.org/976624
Fixes: 9594e6e0aeafcf17f4934e41e3d49d9392b31392
Signed-off-by: Sam James <sam@gentoo.org>

app-containers/docker-compose/docker-compose-5.1.4-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit optfeature
+inherit eapi9-ver optfeature
 
 DESCRIPTION="Multi-container orchestration for Docker"
 HOMEPAGE="https://github.com/docker/compose"