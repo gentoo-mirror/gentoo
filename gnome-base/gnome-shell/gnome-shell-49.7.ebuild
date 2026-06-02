gentoo-mirror/gentoo

commit 2152e3c2b98d382989103714de3e39ec945f2ad2

gnome-base/gnome-shell: Fix docs installation PATH
Closes: https://bugs.gentoo.org/946902
Signed-off-by: Pacho Ramos <pacho@gentoo.org>

gnome-base/gnome-shell/gnome-shell-49.7.ebuild
@@ -189,6 +189,14 @@ src_test() {
 	meson_src_test
 }
 
+src_install() {
+	meson_src_install
+	if use gtk-doc; then
+		mkdir -p "${ED}"/usr/share/gtk-doc/html || die
+		mv "${ED}"/usr/share/doc/s* "${ED}"/usr/share/gtk-doc/html/ || die
+	fi
+}
+
 pkg_postinst() {
 	xdg_pkg_postinst
 	gnome2_schemas_update

commit 90b14927c70eb34f1317fc66ff3566ea96fc8f7d

gnome-base/gnome-shell: fix glob pattern in install phase
`mv "${ED}"/usr/share/doc/s* "${ED}"/usr/share/gtk-doc/html/ || die` moves
everything starting with `s` instead of just `st` and `shell`

Fixes: 2152e3c2b98d
Bug: https://bugs.gentoo.org/946902
Signed-off-by: zyxhere💭 <zyx@envs.net>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1027
Merges: https://codeberg.org/gentoo/gentoo/pulls/1027
Signed-off-by: Sam James <sam@gentoo.org>

gnome-base/gnome-shell/gnome-shell-49.7.ebuild
@@ -193,7 +193,7 @@ src_install() {
 	meson_src_install
 	if use gtk-doc; then
 		mkdir -p "${ED}"/usr/share/gtk-doc/html || die
-		mv "${ED}"/usr/share/doc/s* "${ED}"/usr/share/gtk-doc/html/ || die
+		mv "${ED}"/usr/share/doc/{st,shell} "${ED}"/usr/share/gtk-doc/html/ || die
 	fi
 }
 