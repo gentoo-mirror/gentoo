gentoo-mirror/gentoo

commit f18342b98477f649aab06104b366c382675da921

net-analyzer/fail2ban: use supervise-daemon
I kept hitting a startup failure and couldn't get anywhere with figuring
out what wa shappening, but OpenRC is moving towards preferring supervise-daemon,
so whatever.

For the live ebuild, when adding this patch in, I noticed it failed
to install because of upstream commit 3b8033b3372f9aa9a1855fe59501898cebab60ca,
so add the tmpfiles config to files/.

Signed-off-by: Sam James <sam@gentoo.org>

net-analyzer/fail2ban/files/fail2ban-tmpfiles.conf
@@ -1,0 +1,1 @@
+D /run/fail2ban 0755 root root -

net-analyzer/fail2ban/fail2ban-1.1.0-r7.ebuild
@@ -55,7 +55,13 @@ DOCS=( ChangeLog DEVELOP README.md THANKS TODO doc/run-rootless.txt )
 PATCHES=(
 	"${FILESDIR}"/${PN}-0.11.2-adjust-apache-logs-paths.patch
 	"${FILESDIR}"/${PN}-1.0.2-umask-tests.patch
+	"${FILESDIR}"/${PN}-1.1.0-openssh-9.8.patch
+	"${FILESDIR}"/${PN}-1.1.0-openssh-9.8-fixups.patch
+	"${FILESDIR}"/${PN}-1.1.0-openrc-nftables.patch
 	"${FILESDIR}"/${PN}-1.1.0-systemd-order.patch
+	"${FILESDIR}"/${PN}-1.1.0-postfix-pref.patch
+	"${FILESDIR}"/${PN}-1.1.0-postfix-ddos.patch
+	"${FILESDIR}"/${PN}-1.1.0-sshd-ddos.patch
 	"${FILESDIR}"/${PN}-1.1.0-openrc-use-supervise-daemon.patch
 )
 
@@ -98,7 +104,7 @@ src_install() {
 	newinitd "${S}"/build/fail2ban-openrc.init ${PN}
 	systemd_dounit "${S}"/build/${PN}.service
 
-	dotmpfiles "${FILESDIR}"/${PN}-tmpfiles.conf
+	dotmpfiles files/${PN}-tmpfiles.conf
 
 	doman man/*.{1,5}
 

net-analyzer/fail2ban/files/fail2ban-1.1.0-openrc-use-supervise-daemon.patch
@@ -1,0 +1,23 @@
+I kept hitting a startup failure and couldn't get anywhere with figuring
+out what wa shappening, but OpenRC is moving towards preferring supervise-daemon,
+and this is a bit nicer anyway as it takes any possibility of a race with
+pidfiles out of the equation.
+--- a/files/fail2ban-openrc.init.in
++++ b/files/fail2ban-openrc.init.in
+@@ -33,13 +33,13 @@ FAIL2BAN_SOCKET="${FAIL2BAN_RUNDIR}/${RC_SVCNAME}.sock"
+ # The fail2ban-client program is also capable of starting and stopping
+ # the server, but things are simpler if we let start-stop-daemon do it.
+ command="@BINDIR@/fail2ban-server"
+-pidfile="${FAIL2BAN_RUNDIR}/${RC_SVCNAME}.pid"
+ 
+-# We force the pidfile/socket location in this service script because
++# We force the socket location in this service script because
+ # we're taking responsibility for ensuring that their parent directory
+ # exists and has the correct permissions (which we can't do if the
+ # user is allowed to change them).
+-command_args="${FAIL2BAN_OPTIONS} -p ${pidfile} -s ${FAIL2BAN_SOCKET}"
++supervisor=supervise-daemon
++command_args="${FAIL2BAN_OPTIONS} -f -s ${FAIL2BAN_SOCKET}"
+ retry="30"
+ 
+ depend() {

commit fb514defd4220dc2d4a6276e234c093cced2f25a

net-analyzer/fail2ban: drop tmpfiles
The systemd service uses RuntimeDirectory, and the OpenRC init script
calls checkpath -d.

Fixes: f18342b98477 (2026-04-25; "net-analyzer/fail2ban: use supervise-daemon")
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

net-analyzer/fail2ban/fail2ban-9999.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 PYTHON_COMPAT=( python3_{12..14} )
 
-inherit bash-completion-r1 edo python-single-r1 systemd tmpfiles
+inherit bash-completion-r1 edo python-single-r1 systemd
 
 DESCRIPTION="Scans log files and bans IPs that show malicious signs"
 HOMEPAGE="https://www.fail2ban.org/"
@@ -98,8 +98,6 @@ src_install() {
 	newinitd "${S}"/build/fail2ban-openrc.init ${PN}
 	systemd_dounit "${S}"/build/${PN}.service
 
-	dotmpfiles "${FILESDIR}"/${PN}-tmpfiles.conf
-
 	doman man/*.{1,5}
 
 	# Use INSTALL_MASK if you do not want to touch /etc/logrotate.d.
@@ -119,8 +117,6 @@ pkg_preinst() {
 }
 
 pkg_postinst() {
-	tmpfiles_process ${PN}-tmpfiles.conf
-
 	if [[ ${previous_less_than_0_7} == 0 ]] ; then
 		elog
 		elog "Configuration files are now in /etc/fail2ban/"

net-analyzer/fail2ban/files/fail2ban-tmpfiles.conf
@@ -1,1 +1,0 @@
-D /run/fail2ban 0755 root root -