gentoo-mirror/gentoo

commit 9a9672a55f5a70ce517a64f806ee4e45cd641211

sys-devel/qbe: cleanup most elibc_mingw use
As Ionen pointed out on the PR, it isn't needed for deps because the USE flag
is only ever set on profiles synthesised by crossdev. We can also drop
src_test as we don't currently run tests for cross, though maybe one day
soonish.

Fixes: 5b812fdf1ba1abefa1b057a8da1b6b5ae78fe1be
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/qbe/qbe-9999.ebuild
@@ -23,9 +23,6 @@ SLOT="0"
 IUSE="test"
 RESTRICT="!test? ( test )"
 
-# For qbe in targets like x86_64-w64-mingw32
-BDEPEND="test? ( elibc_mingw? ( virtual/wine ) )"
-
 DOCS=( README doc )
 
 src_prepare() {
@@ -76,14 +73,6 @@ src_compile() {
 	tc-export CC
 
 	emake CFLAGS="-std=c99 ${CPPFLAGS} ${CFLAGS}"
-}
-
-src_test() {
-	if use elibc_mingw; then
-		TARGET=amd64_win default
-	else
-		default
-	fi
 }
 
 src_install() {

profiles/arch/riscv/use.mask
@@ -4,10 +4,6 @@
 # Unmask the flag which corresponds to ARCH.
 -riscv
 
-# Haelwenn (lanodan) Monnier <contact@hacktivis.me> (2026-06-02)
-# mingw isn't keyworded for riscv
-elibc_mingw
-
 # Michael Orlitzky <mjo@gentoo.org> (2024-09-01)
 # These lisp implementations have ~riscv keywords.
 # This counteracts the mask in arch/base/use.mask.

sys-devel/qbe/qbe-1.3.ebuild
@@ -23,9 +23,6 @@ SLOT="0"
 IUSE="test"
 RESTRICT="!test? ( test )"
 
-# For qbe in targets like x86_64-w64-mingw32
-BDEPEND="test? ( elibc_mingw? ( virtual/wine ) )"
-
 DOCS=( README doc )
 
 src_prepare() {
@@ -76,14 +73,6 @@ src_compile() {
 	tc-export CC
 
 	emake CFLAGS="-std=c99 ${CPPFLAGS} ${CFLAGS}"
-}
-
-src_test() {
-	if use elibc_mingw; then
-		TARGET=amd64_win default
-	else
-		default
-	fi
 }
 
 src_install() {

commit 564668ae67b34a2fd764428a6bfe80ec881361f9

sys-devel/qbe: drop obsolete USE=test
It's not necessary anymore now that we don't have any dependencies
based on it.

Fixes: 9a9672a55f5a70ce517a64f806ee4e45cd641211
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/qbe/qbe-1.3-r1.ebuild
@@ -20,8 +20,6 @@ HOMEPAGE="https://c9x.me/compile/"
 
 LICENSE="MIT"
 SLOT="0"
-IUSE="test"
-RESTRICT="!test? ( test )"
 
 DOCS=( README doc )
 

sys-devel/qbe/qbe-9999.ebuild
@@ -20,8 +20,6 @@ HOMEPAGE="https://c9x.me/compile/"
 
 LICENSE="MIT"
 SLOT="0"
-IUSE="test"
-RESTRICT="!test? ( test )"
 
 DOCS=( README doc )
 