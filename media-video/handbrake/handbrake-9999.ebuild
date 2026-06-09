gentoo-mirror/gentoo

commit 91e4318e7443439f2d9e41b49328ff915f0700f7

media-video/handbrake: update patch to apply against upstream changes
Upstream commit: 86516e6b91ca5406db783b3ae88178d53757a935

Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1080
Signed-off-by: Sam James <sam@gentoo.org>

media-video/handbrake/handbrake-9999.ebuild
@@ -124,7 +124,7 @@ fi
 PATCHES=(
 	"${FILESDIR}"/handbrake-1.9.0-link-libdovi-properly.patch
 	"${FILESDIR}"/handbrake-1.9.0-include-vpl-properly.patch
-	"${FILESDIR}"/handbrake-1.9.2-set-ffmpeg-toolchain-explicitly.patch
+	"${FILESDIR}"/handbrake-1.11.2-set-ffmpeg-toolchain-explicitly.patch
 	"${FILESDIR}"/handbrake-1.9.2-allow-overriding-tools-via-env.patch
 )
 

media-video/handbrake/files/handbrake-1.11.2-set-ffmpeg-toolchain-explicitly.patch
@@ -1,0 +1,36 @@
+From 7e670380373eb5be0c977f467c102eb1210951b5 Mon Sep 17 00:00:00 2001
+From: Alfred Wingate <parona@protonmail.com>
+Date: Wed, 25 Dec 2024 19:49:40 +0200
+Subject: [PATCH 1/2] Set toolchain explicitly for ffmpeg
+
+Signed-off-by: Alfred Wingate <parona@protonmail.com>
+--- a/contrib/ffmpeg/module.defs
++++ b/contrib/ffmpeg/module.defs
+@@ -76,7 +76,13 @@ FFMPEG.CONFIGURE.extra = \
+     --enable-filter=aformat \
+     --enable-filter=acompressor \
+     --enable-filter=agate \
+-    --cc="$(FFMPEG.GCC.gcc)"
++    --cc="$(FFMPEG.GCC.gcc)" \
++    --cxx="$(FFMPEG.GCC.gxx)" \
++    --ar="$(AR.exe)" \
++    --nm="$(NM.exe)" \
++    --strip="$(STRIP.exe)" \
++    --ranlib="$(RANLIB.exe)" \
++    --pkg-config="$(PKGCONFIG.exe)"
+ 
+ ifeq (size-aggressive,$(GCC.O))
+ FFMPEG.CONFIGURE.extra += \
+--- a/make/configure.py
++++ b/make/configure.py
+@@ -1665,6 +1665,7 @@ try:
+         strip      = ToolProbe( 'STRIP.exe',      'strip',      'strip', abort=True )
+         tar        = ToolProbe( 'TAR.exe',        'tar',        'gtar', 'tar', abort=True )
+         python     = ToolProbe( 'PYTHON.exe',     'python',     os.path.basename(sys.executable), abort=True )
++        nm         = ToolProbe( 'NM.exe',         'nm',         'nm', abort=True )
+ 
+         gcc_tools  = ['GCC.gcc',
+                       'cc',
+-- 
+2.47.1
+

commit f1c2512c8e60664bf4ff73a364e12e47fb4865cc

media-video/handbrake: rename patch
Version assumption was wrong.

Fixes: 91e4318e7443439f2d9e41b49328ff915f0700f7
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1111
Merges: https://codeberg.org/gentoo/gentoo/pulls/1111
Signed-off-by: Sam James <sam@gentoo.org>

media-video/handbrake/handbrake-9999.ebuild
@@ -124,7 +124,7 @@ fi
 PATCHES=(
 	"${FILESDIR}"/handbrake-1.9.0-link-libdovi-properly.patch
 	"${FILESDIR}"/handbrake-1.9.0-include-vpl-properly.patch
-	"${FILESDIR}"/handbrake-1.11.2-set-ffmpeg-toolchain-explicitly.patch
+	"${FILESDIR}"/handbrake-1.12.0-set-ffmpeg-toolchain-explicitly.patch
 	"${FILESDIR}"/handbrake-1.9.2-allow-overriding-tools-via-env.patch
 )
 