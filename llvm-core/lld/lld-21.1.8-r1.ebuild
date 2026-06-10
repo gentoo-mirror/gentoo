gentoo-mirror/gentoo

commit 052524cdbebac5738097c1d483d72f578977ded1

llvm-core/lld: workaround >= GCC 14 bug on arm64
It's fixed for >=15.3 but it's easier to have a wider condition for now.

Bug: https://gcc.gnu.org/PR121495
Closes: https://bugs.gentoo.org/958469
Signed-off-by: Sam James <sam@gentoo.org>

llvm-core/lld/lld-21.1.8-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -61,6 +61,9 @@ src_unpack() {
 src_configure() {
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
+
+	# https://gcc.gnu.org/PR121495 (bug #958469)
+	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
 
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 

llvm-core/lld/lld-22.1.7-r1.ebuild
@@ -62,6 +62,9 @@ src_configure() {
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
+	# https://gcc.gnu.org/PR121495 (bug #958469)
+	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 
 	local mycmakeargs=(

llvm-core/lld/lld-23.0.0.9999.ebuild
@@ -61,6 +61,9 @@ src_configure() {
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
+	# https://gcc.gnu.org/PR121495 (bug #958469)
+	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 
 	local mycmakeargs=(

llvm-core/lld/lld-23.0.0_pre20260605.ebuild
@@ -61,6 +61,9 @@ src_configure() {
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
+	# https://gcc.gnu.org/PR121495 (bug #958469)
+	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 
 	local mycmakeargs=(

commit 943515aa092eacdb79f6be8f7ad6c4200d059c05

llvm-core/lld: only add -mearly-ra=none for arm64
Bug: https://bugs.gentoo.org/958469
Fixes: 052524cdbebac5738097c1d483d72f578977ded1
Signed-off-by: Sam James <sam@gentoo.org>

llvm-core/lld/lld-21.1.8-r1.ebuild
@@ -63,7 +63,7 @@ src_configure() {
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
 	# https://gcc.gnu.org/PR121495 (bug #958469)
-	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+	use arm64 && tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
 
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 

llvm-core/lld/lld-22.1.7-r1.ebuild
@@ -63,7 +63,7 @@ src_configure() {
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
 	# https://gcc.gnu.org/PR121495 (bug #958469)
-	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+	use arm64 && tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
 
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 

llvm-core/lld/lld-23.0.0.9999.ebuild
@@ -62,7 +62,7 @@ src_configure() {
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
 	# https://gcc.gnu.org/PR121495 (bug #958469)
-	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+	use arm64 && tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
 
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 

llvm-core/lld/lld-23.0.0_pre20260605.ebuild
@@ -62,7 +62,7 @@ src_configure() {
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 
 	# https://gcc.gnu.org/PR121495 (bug #958469)
-	tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
+	use arm64 && tc-is-gcc && [[ $(gcc-major-version) -ge 14 ]] && append-flags -mearly-ra=none
 
 	use elibc_musl && append-ldflags -Wl,-z,stack-size=2097152
 