gentoo-mirror/gentoo

commit e0f62b2d78355565c25c27c8e811740b62959761

sys-apps/coreutils: respect enable_year2038 set in profiles
Closes: https://bugs.gentoo.org/917311
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/coreutils/coreutils-9999.ebuild
@@ -23,7 +23,7 @@ if [[ ${PV} == 9999 ]] ; then
 elif [[ ${PV} == *_p* ]] ; then
 	# Note: could put this in devspace, but if it's gone, we don't want
 	# it in tree anyway. It's just for testing.
-	MY_SNAPSHOT="$(ver_cut 1-2).289-a8598"
+	MY_SNAPSHOT="$(ver_cut 1-2).299-27a7c"
 	SRC_URI="https://www.pixelbeat.org/cu/coreutils-${MY_SNAPSHOT}.tar.xz -> ${P}.tar.xz"
 	SRC_URI+=" verify-sig? ( https://www.pixelbeat.org/cu/coreutils-${MY_SNAPSHOT}.tar.xz.sig -> ${P}.tar.xz.sig )"
 	S="${WORKDIR}"/${PN}-${MY_SNAPSHOT}
@@ -173,6 +173,11 @@ src_configure() {
 	if use gmp ; then
 		myconf+=( --with-libgmp-prefix="${ESYSROOT}"/usr )
 	fi
+
+	# https://savannah.gnu.org/support/?111394
+	# This can be removed when we patch dev-build/autoconf, though
+	# packages w/o eautoreconf will still need it.
+	[[ ${enable_year2038} == "no" ]] && myconf+=( --disable-year2038 )
 
 	if tc-is-cross-compiler && [[ ${CHOST} == *linux* ]] ; then
 		# bug #311569

sys-apps/coreutils/coreutils-9.11.ebuild
@@ -174,6 +174,11 @@ src_configure() {
 		myconf+=( --with-libgmp-prefix="${ESYSROOT}"/usr )
 	fi
 
+	# https://savannah.gnu.org/support/?111394
+	# This can be removed when we patch dev-build/autoconf, though
+	# packages w/o eautoreconf will still need it.
+	[[ ${enable_year2038} == "no" ]] && myconf+=( --disable-year2038 )
+
 	if tc-is-cross-compiler && [[ ${CHOST} == *linux* ]] ; then
 		# bug #311569
 		export fu_cv_sys_stat_statfs2_bsize=yes

commit 21a3b62ff0c57c7c2661a947dcb82911f09a93bd

sys-apps/coreutils: fix tests w/ 32-bit time_t
Bug: https://bugs.gentoo.org/917311
Fixes: e0f62b2d78355565c25c27c8e811740b62959761
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/coreutils/coreutils-9.11-r1.ebuild
@@ -249,6 +249,8 @@ src_test() {
 		)
 	fi
 
+	[[ ${enable_year2038} == "no" ]] && xfail_tests+=( test-year2038 )+
+
 	# This test is flaky (bug #910640).
 	cat > tests/tty/tty-eof.pl <<-EOF || die
 	#!/usr/bin/perl

sys-apps/coreutils/coreutils-9999.ebuild
@@ -248,6 +248,8 @@ src_test() {
 		)
 	fi
 
+	[[ ${enable_year2038} == "no" ]] && xfail_tests+=( test-year2038 )
+
 	# This test is flaky (bug #910640).
 	cat > tests/tty/tty-eof.pl <<-EOF || die
 	#!/usr/bin/perl